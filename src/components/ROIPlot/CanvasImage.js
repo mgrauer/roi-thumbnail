c59a1a31265b4af984c60566869acec34a4b8e21
Roni Choudhury <roni.choudhury@kitware.com>
2018-11-16 15:02:38 -0500

    Abstract logic for canvas display into own class

---
+++ src/components/ROIPlot/CanvasImage.js
@@ -0,0 +1,51 @@
+export default class CanvasImage {
+  constructor (el, {width, height}) {
+    this.el = el;
+    this.width = width;
+    this.height = height;
+
+    this.ctx = this.el.getContext('2d');
+
+    this.img = this.ctx.createImageData(this.width, this.height);
+  }
+
+  clear (r, g, b, a, { update = true } = {}) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        this.setPixel(i, j, { r, g, b, a, update: false });
+      }
+    }
+
+    if (update) {
+      this.update();
+    }
+  }
+
+  setPixel (i, j, {r, g, b, a, update = true}) {
+    const idx = i * this.width + j;
+
+    if (r !== undefined) {
+      this.img.data[4 * idx + 0] = r;
+    }
+
+    if (g !== undefined) {
+      this.img.data[4 * idx + 1] = g;
+    }
+
+    if (b !== undefined) {
+      this.img.data[4 * idx + 2] = b;
+    }
+
+    if (a !== undefined) {
+      this.img.data[4 * idx + 3] = a;
+    }
+
+    if (update) {
+      this.update();
+    }
+  }
+
+  update () {
+    this.ctx.putImageData(this.img, 0, 0);
+  }
+}
--- src/components/ROIPlot/script.js
+++ src/components/ROIPlot/script.js
@@ -1,3 +1,5 @@
+import CanvasImage from './CanvasImage';
+
 export default {
   name: 'ROIPlot',
   computed: {
@@ -17,32 +19,23 @@ export default {
     const el = this.$el;
     const canvas = el.getElementsByTagName('canvas')[0];
 
-    const ctx = canvas.getContext('2d');
+    this.img = new CanvasImage(canvas, {
+      width: this.width,
+      height: this.height
+    });
 
-    const img = ctx.createImageData(this.width, this.height);
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        const idx = i * this.width + j;
-
-        img.data[4 * idx + 0] = 0;
-        img.data[4 * idx + 1] = 0;
-        img.data[4 * idx + 2] = 0;
-        img.data[4 * idx + 3] = 255;
-      }
-    }
+    this.img.clear(0, 0, 0, 255);
 
     const rois = this.rois;
-
     for (let i = 0; i < rois.length; i++) {
       for (let j = 0; j < rois[i].length; j++) {
-        const x = rois[i][j][0];
-        const y = rois[i][j][1];
-        const idx = x * this.width + y;
-
-        img.data[4 * idx + 1] = 255;
+        this.img.setPixel(rois[i][j][0], rois[i][j][1], {
+          g: 255,
+          update: false
+        });
       }
     }
 
-    ctx.putImageData(img, 0, 0);
+    this.img.update();
   }
 }
